9a706378d4535661c9a6b0ffe45104ca691d7de0
Ebrahim Elsayed Elgharib <ebrahimelsayed010.com>
2023-09-16 06:05:56 +0300

    add custom admin for product aap && admin_tabular

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product, ProductImages, Brand, Review
+
+
+
+class ProductImagesTabular(admin.TabularInline):
+    model = ProductImages
+
+    
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'flag', 'price', 'quantity', 'brand']
+    list_filter = ['flag', 'brand']
+    search_fields = ['name', 'subtitle']
+    
+    inlines = [ProductImagesTabular]
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductImages)
+admin.site.register(Brand)
+admin.site.register(Review)
