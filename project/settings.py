78ed577860ce75b4e332a68dcfcc5c1247d941c4
Ebrahim Elsayed Elgharib <ebrahimelsayed010.com>
2023-10-20 17:29:13 +0300

    install rest-framework && txt requirements.txt

--- project/settings.py
+++ project/settings.py
@@ -38,14 +38,23 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    'rest_framework',
+    
     "debug_toolbar",
     "taggit",
+    
     
     
     'product',
     'settings',
     
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
